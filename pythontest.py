f87a0c28fc667e07b352f1f6e6985dbe757f6f47
Peter <peterjohnm2002@gmail.com>
2022-06-11 01:11:30 -0500

    even more test files and test code

---
+++ pythontest.py
@@ -0,0 +1,7 @@
+# first_number = input ("First: ")
+# second_number = input ("Second: ")
+# sum = float(first_number) + float(second_number)
+# print("Sum:", sum)
+course = "test test test"
+print(course.replace("test","yeet"))
+course.find_all()
--- test.cpp
+++ test.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main() {
     cout << "test" << endl;
-
+    string x = "10 ";
+    x += "test";
+    cout << x << endl;
     return 0;
 }
